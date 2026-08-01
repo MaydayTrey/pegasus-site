76f6721508811d37a85d00614777dfba4c284e91
Trey <233525373+MaydayTrey@users.noreply.github.com>
2026-08-01 17:14:28 -0400

    Rough day, but did the modal for about.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dialog = document.getElementById("about-dialog");
 const dialogContent = document.getElementById("dialog-content");
 const closeBtn = dialog.querySelector(".dialog-close");
 const cards = document.querySelectorAll(".about-card");
+const shadowforeground = document.querySelector(".shadow-foreground");
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
@@ -16,12 +17,24 @@ cards.forEach((card) => {
     });
 
     dialog.showModal();
+    const foregroundCurOpacity = parseFloat(
+      window.getComputedStyle(shadowforeground).opacity
+    );
+    if (foregroundCurOpacity === 0) {
+      shadowforeground.style.opacity = "0.3";
+    }
   });
 });
 
 dialog.addEventListener("click", (e) => {
   if (!dialogContent.contains(e.target) && e.target !== dialogContent) {
     dialog.close();
+    const foregroundCurOpacity = parseFloat(
+      window.getComputedStyle(shadowforeground).opacity
+    );
+    if (foregroundCurOpacity === 0.3) {
+      shadowforeground.style.opacity = "0";
+    }
   }
 });
 
