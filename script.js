30229daf97a259c16bdaa24f14bb11a763bef762
Trey <233525373+MaydayTrey@users.noreply.github.com>
2026-08-01 17:30:22 -0400

    Decided to do a split branch - one for deployment, one for learning.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const dialog = document.getElementById("about-dialog");
 const dialogContent = document.getElementById("dialog-content");
 const closeBtn = dialog.querySelector(".dialog-close");
 const cards = document.querySelectorAll(".about-card");
-const shadowforeground = document.querySelector(".shadow-foreground");
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
@@ -17,24 +16,12 @@ cards.forEach((card) => {
     });
 
     dialog.showModal();
-    const foregroundCurOpacity = parseFloat(
-      window.getComputedStyle(shadowforeground).opacity
-    );
-    if (foregroundCurOpacity === 0) {
-      shadowforeground.style.opacity = "0.3";
-    }
   });
 });
 
 dialog.addEventListener("click", (e) => {
   if (!dialogContent.contains(e.target) && e.target !== dialogContent) {
     dialog.close();
-    const foregroundCurOpacity = parseFloat(
-      window.getComputedStyle(shadowforeground).opacity
-    );
-    if (foregroundCurOpacity === 0.3) {
-      shadowforeground.style.opacity = "0";
-    }
   }
 });
 
